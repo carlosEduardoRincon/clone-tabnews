b2c954eccacc867963467d7c2fb6ea315705b69c
Carlos Eduardo Rincon Junior <85880891+carlosEduardoRincon@users.noreply.github.com>
2023-06-12 21:14:09 +0000

    Some changes

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,10 @@ function Home() {
     <div>
         <h1>Para o meu amor.</h1>
         <p>
-            Como eu te disse, eu quero compartilhar com você 
-            cada evolução e cada progresso realizado na minha vida.
-            Bom, hoje hospedei o meu primeiro site, o qual contem 
-            essa mensagem simples que ficará para sempre (literalmente)
-            na Internet, e além disso qualquer pessoa pode ver.
+            Bom... para um momento especial exige-se músicas especiais.
+            Por isso, criei uma playlist para comemoramos hehe
         </p>
-        <p>
-            Bom, esse espaço no mundo da internet não é só meu, mas nosso.
-            Obrigado Isadora (não me mata) por ser tão especial na minha vida,
-            espero que um dia nossos filhos possam ver isso, e notar que o pai 
-            deles é um tonto apaixonado por você hehe
-        </p>
-        <h2>Eu te amo minha vida! E novamente, feliz dia dos namorados</h2>
-        <p>Obs: Ignore a simplicidade, coisas melhores virão</p>
+        <a href="https://open.spotify.com/playlist/76zkn544hsiPA3ZJm4xC1b?si=80473147435e4671&pt=1fb179cce4c9e2a229f8ecaa6636bd9f">Clicke aqui! Espero que goste</a>
     </div>
     );
 }
