357f711e28ec6e9e22061eb95cfc6369fbc57627
Carlos Eduardo Rincon Junior <85880891+carlosEduardoRincon@users.noreply.github.com>
2023-06-12 16:25:43 +0000

    Message to especial person

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
 function Home() {
-    return <h1>É só o começo! Novos aprendizados estão por vim...</h1>
+    return (
+    <div>
+        <h1>Para o meu amor.</h1>
+        <p>
+            Como eu te disse, eu quero compartilhar com você 
+            cada evolução e cada progresso realizado na minha vida.
+            Bom, hoje hospedei o meu primeiro site, o qual contem 
+            essa mensagem simples que ficará para sempre (literalmente)
+            na Internet, e além disso qualquer pessoa pode ver.
+        </p>
+        <p>
+            Bom, esse espaço no mundo da internet não é só meu, mas nosso.
+            Obrigado Isadora (não me mata) por ser tão especial na minha vida,
+            espero que um dia nossos filhos possam ver isso, e notar que o pai 
+            deles é um tonto apaixonado por você hehe
+        </p>
+        <h2>Eu te amo minha vida! E novamente, feliz dia dos namorados</h2>
+        <p>Obs: Ignore a simplicidade, coisas melhores virão</p>
+    </div>
+    );
 }
 
 export default Home;
