0733aefb95bd1ba7cad857e9cf4e034c7e3d38fe
Carlos Eduardo Rincon Junior <85880891+carlosEduardoRincon@users.noreply.github.com>
2023-08-10 02:41:58 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do tabnews para aprimorar conceitos de programação
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 function Home() {
-    return (
+  return (
     <div>
-        <h1>Uma pequena surpresa para a minha vida.</h1>
-        <p>
-            Bom... para um momento especial exige-se músicas especiais.
-            Por isso, criei uma playlist para comemoramos hehe
-        </p>
-        <a href="https://open.spotify.com/playlist/76zkn544hsiPA3ZJm4xC1b?si=80473147435e4671&pt=1fb179cce4c9e2a229f8ecaa6636bd9f">Clicke aqui! Espero que goste</a>
+      <h1>Uma pequena surpresa para a minha vida.</h1>
+      <p>
+        Bom... para um momento especial exige-se músicas especiais. Por isso,
+        criei uma playlist para comemoramos hehe
+      </p>
+      <a href="https://open.spotify.com/playlist/76zkn544hsiPA3ZJm4xC1b?si=80473147435e4671&pt=1fb179cce4c9e2a229f8ecaa6636bd9f">
+        Clicke aqui! Espero que goste
+      </a>
     </div>
-    );
+  );
 }
 
 export default Home;
