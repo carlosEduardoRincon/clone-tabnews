fb6e441a029cc1884cc2743991e8960cc5e471ad
Carlos Eduardo Rincon Junior <85880891+carlosEduardoRincon@users.noreply.github.com>
2024-02-07 11:06:50 +0000

    Real tests and mock funciton

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,21 @@
-test("Expect that 1 is 1", () => {
-  expect(1).toBe(1);
+const calculadora = require("../models/calculadora.js");
+
+test("Sum test 2+3 is 5", () => {
+  expect(calculadora.somar(2, 3)).toBe(5);
+});
+
+test("Sum test 5+100 is 105", () => {
+  expect(calculadora.somar(5, 100)).toBe(105);
+});
+
+test("Sum test curso+100 is Error", () => {
+  expect(calculadora.somar("curso", 100)).toBe("Erro");
+});
+
+test("Sum test 100+curso is Error", () => {
+  expect(calculadora.somar(100, "curso")).toBe("Erro");
+});
+
+test('Sum test ""+curso is Error', () => {
+  expect(calculadora.somar("", "curso")).toBe("Erro");
 });
