870324401b31a788472bce80813cca04d7cf0eed
Carlos Eduardo Rincon Junior <85880891+carlosEduardoRincon@users.noreply.github.com>
2024-03-25 22:06:47 +0000

    feat - addingpostgres info

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,8 +2,17 @@ import database from "/infra/database.js";
 
 async function status(request, response) {
   const updateAt = new Date().toISOString();
+  const postgresVersion = await database.query("SELECT version();");
+  const maxConnections = await database.query("show max_connections;");
+  const usedConnections = await database.query(
+    "SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'active';",
+  );
+
   response.status(200).json({
     updated_at: updateAt,
+    postgres_version: postgresVersion.rows[0].version,
+    max_connections: maxConnections.rows[0].max_connections,
+    used_connections: usedConnections.rowCount,
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,11 +1,17 @@
 test("Get /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status/");
   expect(response.status).toBe(200);
-
   const responseBody = await response.json();
   console.log(responseBody);
+
   expect(responseBody.updated_at).toBeDefined();
 
   const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+
+  expect(responseBody.postgres_version).toBeDefined();
+
+  expect(responseBody.max_connections).toBeDefined();
+
+  expect(responseBody.used_connections).toBeDefined();
 });
