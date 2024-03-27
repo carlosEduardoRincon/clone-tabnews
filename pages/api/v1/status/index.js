7e79da9c3342dfea3efaa183414ce01ea22abd89
Carlos Eduardo Rincon Junior <85880891+carlosEduardoRincon@users.noreply.github.com>
2024-03-27 14:19:17 +0000

    fix - finish status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,17 +2,24 @@ import database from "/infra/database.js";
 
 async function status(request, response) {
   const updateAt = new Date().toISOString();
-  const postgresVersion = await database.query("SELECT version();");
+  const postgresVersion = await database.query("show server_version;");
   const maxConnections = await database.query("show max_connections;");
-  const usedConnections = await database.query(
-    "SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'active';",
-  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const usedConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
 
   response.status(200).json({
     updated_at: updateAt,
-    postgres_version: postgresVersion.rows[0].version,
-    max_connections: maxConnections.rows[0].max_connections,
-    used_connections: usedConnections.rowCount,
+    dependencies: {
+      database: {
+        version: postgresVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: usedConnections.rows[0].count,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -9,9 +9,16 @@ test("Get /api/v1/status should return 200", async () => {
   const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdateAt);
 
-  expect(responseBody.postgres_version).toBeDefined();
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
 
-  expect(responseBody.max_connections).toBeDefined();
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
 
-  expect(responseBody.used_connections).toBeDefined();
+  expect(responseBody.dependencies.database.opened_connections).toEqual(3);
 });
+
+/*test("Teste de SQL Injection /api/v1/status", async () => {
+  const response = await fetch(
+    "http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4); --",
+  );
+  expect(response.status).toBe(200);
+});*/
