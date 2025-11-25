7eea941b9ab7f624b99a947b47fcf4a518b61fa4
Carlos Eduardo Rincon Junior <85880891+carlosEduardoRincon@users.noreply.github.com>
2025-11-25 22:27:59 +0000

    fix - adjusts migration endpoint and jest dependency

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -7,18 +7,9 @@ test("Get /api/v1/status should return 200", async () => {
   expect(responseBody.updated_at).toBeDefined();
 
   const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+
   expect(responseBody.updated_at).toEqual(parsedUpdateAt);
-
   expect(responseBody.dependencies.database.version).toEqual("16.0");
-
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
-
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
-
-/*test("Teste de SQL Injection /api/v1/status", async () => {
-  const response = await fetch(
-    "http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4); --",
-  );
-  expect(response.status).toBe(200);
-});*/
