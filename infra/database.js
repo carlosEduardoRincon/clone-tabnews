db1e8af00e5ae1f6269f438c465de9eabb87b032
Carlos Eduardo Rincon Junior <85880891+carlosEduardoRincon@users.noreply.github.com>
2025-11-26 00:41:58 +0000

    feat - adjust at jest and adding query to clean database

--- infra/database.js
+++ infra/database.js
@@ -10,14 +10,6 @@ async function query(queryObject) {
     ssl: getSSLValues(),
   });
 
-  console.log("Credenciais do Postgres:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-
   try {
     await client.connect();
     const result = await client.query(queryObject);
@@ -39,5 +31,5 @@ function getSSLValues() {
     return {
       ca: process.env.POSTGRES_CA,
     };
-  return process.env.NODE_ENV == "development" ? false : true;
+  return process.env.NODE_ENV === "production" ? true : false;
 }
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,11 +6,12 @@ async function cleanDatabase() {
   await database.query("drop schema public cascade; create schema public;");
 }
 
-test("GET /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations/");
+test("POST /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations/", {
+    method: "POST",
+  });
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
   expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
 });
