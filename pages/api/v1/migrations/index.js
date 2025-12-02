eb1af2e3d68aa59afa3b32bd10a654c395d3576a
Carlos Eduardo Rincon Junior <85880891+carlosEduardoRincon@users.noreply.github.com>
2025-12-02 23:38:12 +0000

    fix: improving index class and adjust test

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,29 +2,31 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  if (request.method == "GET") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
+  const defaultMigrtationOptioons = {
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
 
-    return response.status(200).json(migrations);
+  if (request.method == "GET") {
+    const pendingMigration = await migrationRunner(defaultMigrtationOptioons);
+
+    return response.status(200).json(pendingMigration);
   }
 
   if (request.method == "POST") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrtationOptioons,
+      dryRun: false,
     });
-    return response.status(200).json(migrations);
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+    return response.status(200).json(migratedMigrations);
   }
 
   return response.status(405);
